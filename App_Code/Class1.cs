cad1d9e745725f7965b1f7b9608890313e8753a3
Kayla Wesley <kaylawsl@gmail.com>
2016-02-05 14:08:29 -0600

    Empty Web Site

---
+++ App_Code/Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// This is for testing openNPL
+/// </summary>
+namespace NaturalLanguageProcessingCSharp
+{
+    public class EntityExtractor
+    {
+
+        private string sentenceModelPath = HttpContext.Current.Server.MapPath("~/Bin/da-sent.bin");
+        private string nameFinderModelPath;
+        private string tokenModelPath = HttpContext.Current.Server.MapPath("~/Bin/da-token.bin");
+        public enum EntityType
+        {
+            Date = 0,
+            Location,
+            Money,
+            Organization,
+            Person,
+            Time
+        }
+        public List<string> ExtractEntities(string inputData, EntityType targetType)
+        {
+            /*required steps to detect names are:
+             * downloaded sentence, token, and name models from http://opennlp.sourceforge.net/models-1.5/
+             * 1. Parse the input into sentences
+             * 2. Parse the sentences into tokens
+             * 3. Find the entity in the tokens
+ 
+            */
+
+            //------------------Preparation -- Set Name Finder model path based upon entity type-----------------
+            switch (targetType)
+            {
+                case EntityType.Date:
+                    nameFinderModelPath = HttpContext.Current.Server.MapPath("~/Bin/en-ner-date.bin");
+                    break;
+                case EntityType.Location:
+                    nameFinderModelPath = HttpContext.Current.Server.MapPath("~/Bin/en-ner-location.bin");
+                    break;
+                case EntityType.Money:
+                    nameFinderModelPath = HttpContext.Current.Server.MapPath("~/Bin/en-ner-money.bin");
+                    break;
+                case EntityType.Organization:
+                    nameFinderModelPath = HttpContext.Current.Server.MapPath("~/Bin/en-ner-organization.bin");
+                    break;
+                case EntityType.Person:
+                    nameFinderModelPath = HttpContext.Current.Server.MapPath("~/Bin/en-ner-person.bin");
+                    break;
+                case EntityType.Time:
+                    nameFinderModelPath = HttpContext.Current.Server.MapPath("~/Bin/en-ner-time.bin");
+                    break;
+                default:
+                    break;
+            }
+
+            //----------------- Preparation -- load models into objects-----------------
+            //initialize the sentence detector
+            opennlp.tools.sentdetect.SentenceDetectorME sentenceParser = prepareSentenceDetector();
+
+            //initialize person names model
+            opennlp.tools.namefind.NameFinderME nameFinder = prepareNameFinder();
+
+            //initialize the tokenizer--used to break our sentences into words (tokens)
+            opennlp.tools.tokenize.TokenizerME tokenizer = prepareTokenizer();
+
+            //------------------  Make sentences, then tokens, then get names--------------------------------
+
+            String[] sentences = sentenceParser.sentDetect(inputData); //detect the sentences and load into sentence array of strings
+            List<string> results = new List<string>();
+
+            foreach (string sentence in sentences)
+            {
+                //now tokenize the input.
+                //"Don Krapohl enjoys warm sunny weather" would tokenize as
+                //"Don", "Krapohl", "enjoys", "warm", "sunny", "weather"
+                string[] tokens = tokenizer.tokenize(sentence);
+
+                //do the find
+                opennlp.tools.util.Span[] foundNames = nameFinder.find(tokens);
+
+                //important:  clear adaptive data in the feature generators or the detection rate will decrease over time.
+                nameFinder.clearAdaptiveData();
+
+                results.AddRange(opennlp.tools.util.Span.spansToStrings(foundNames, tokens).AsEnumerable());
+            }
+
+            return results;
+        }
+
+        #region private methods
+        private opennlp.tools.tokenize.TokenizerME prepareTokenizer()
+        {
+            java.io.FileInputStream tokenInputStream = new java.io.FileInputStream(tokenModelPath);     //load the token model into a stream
+            opennlp.tools.tokenize.TokenizerModel tokenModel = new opennlp.tools.tokenize.TokenizerModel(tokenInputStream); //load the token model
+            return new opennlp.tools.tokenize.TokenizerME(tokenModel);  //create the tokenizer
+        }
+        private opennlp.tools.sentdetect.SentenceDetectorME prepareSentenceDetector()
+        {
+            java.io.FileInputStream sentModelStream = new java.io.FileInputStream(sentenceModelPath);       //load the sentence model into a stream
+            opennlp.tools.sentdetect.SentenceModel sentModel = new opennlp.tools.sentdetect.SentenceModel(sentModelStream);// load the model
+            return new opennlp.tools.sentdetect.SentenceDetectorME(sentModel); //create sentence detector
+        }
+        private opennlp.tools.namefind.NameFinderME prepareNameFinder()
+        {
+            java.io.FileInputStream modelInputStream = new java.io.FileInputStream(nameFinderModelPath); //load the name model into a stream
+            opennlp.tools.namefind.TokenNameFinderModel model = new opennlp.tools.namefind.TokenNameFinderModel(modelInputStream); //load the model
+            return new opennlp.tools.namefind.NameFinderME(model);                   //create the namefinder
+        }
+        #endregion
+
+
+    }
+}
---
+++ Default.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<%@ Import Namespace="System.IO"%>
+<%@ Import Namespace="NaturalLanguageProcessingCSharp"%>
+
+<!DOCTYPE html>
+<script runat="server">
+
+    void Page_Load(Object sender, EventArgs e)
+    {
+        
+        EntityExtractor seeker = new EntityExtractor();
+        String line;
+        try
+        {
+            using (StreamReader sr = new StreamReader(MapPath("TestCases/testText2.txt")))
+            {
+                line = sr.ReadToEnd();
+                Console.WriteLine(line);
+                Response.Write(line);
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(ex.Message);
+            line = "";
+        }
+        List<String> ppl = seeker.ExtractEntities(line, EntityExtractor.EntityType.Person);
+        foreach(String s in ppl)
+        {
+            Response.Write("/n");
+            Response.Write(s);
+        }
+        /*List<String> org = seeker.ExtractEntities(line, EntityExtractor.EntityType.Organization);
+        foreach (String s in org)
+        {
+            Response.Write(s);
+        }
+        List<String> loc = seeker.ExtractEntities(line, EntityExtractor.EntityType.Location);
+        foreach (String s in loc)
+        {
+            Response.Write(s);
+        }
+        List<String> mon = seeker.ExtractEntities(line, EntityExtractor.EntityType.Money);
+        foreach (String s in mon)
+        {
+            Response.Write(s);
+        }
+        List<String> tm = seeker.ExtractEntities(line, EntityExtractor.EntityType.Time);
+        foreach (String s in tm)
+        {
+            Response.Write(s);
+        }
+        List<String> dt = seeker.ExtractEntities(line, EntityExtractor.EntityType.Date);
+        foreach (String s in dt)
+        {
+            Response.Write(s);
+        }*/
+    }
+
+</script>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        <p>
+
+        </p>
+    </div>
+    </form>
+</body>
+</html>
